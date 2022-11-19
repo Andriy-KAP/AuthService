089f4e75d070c33666b0d00c9b06277f3d8c4339
Andriy-KAP <andriykuklinov@gmail.com>
2022-11-19 20:29:19 +0200

    Adding IdentittyServer config file.

---
+++ AuthService/AuthService/Config.cs
@@ -0,0 +1,26 @@
+﻿using IdentityServer4.Models;
+
+namespace AuthService
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiScope> ApiScope => new List<ApiScope>
+        {
+            new ApiScope("api1", "My API")
+        };
+
+        public static IEnumerable<Client> Clients => new List<Client>
+        {
+            new Client
+            {
+                ClientId = "client",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+                AllowedScopes = { "api1" }
+            }
+        };
+    }
+}
---
+++ AuthService/AuthService/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace AuthService
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+        public void ConfigureService(IServiceCollection services)
+        {
+            services.AddId
+            services.AddMvc();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}"
+                );
+            });
+            
+        }
+    }
+}
